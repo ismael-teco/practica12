d101d7fa5e2248aef7484f7e04556d8812260b80
ismael-teco <60282039+ismael-teco@users.noreply.github.com>
2020-03-18 23:58:46 -0500

    Add files via upload

---
+++ practica12/procesar.php
@@ -0,0 +1,24 @@
+
+<link rel="stylesheet" href="css/style.css">
+<body>
+<?php
+
+$nombreAlumno = $_POST['nombre'];
+$apellidosAlumno = $_POST['apellidos'];
+
+echo '<p>tu nombre es:</p>'.'<p>&nbsp'.$nombreAlumno.'&nbsp'. $apellidosAlumno.'</p>';
+
+
+
+
+/*ismael teco medina
+        5 J */ 
+
+?>
+
+<div>
+
+<input type="button" value="Volver " onclick="history.back()" style="font-family: Verdana; font-size: 10 pt">
+     
+    </div>
+</body>
